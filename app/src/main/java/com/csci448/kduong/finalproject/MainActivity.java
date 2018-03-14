031ff2113cb4cc795c0761d7e7fa9d03bcee4174
The Ngo <thngo@mymail.mines.edu>
2018-03-14 11:51:48 -0600

    Added the profile page

--- app/src/main/java/com/csci448/kduong/finalproject/MainActivity.java
+++ app/src/main/java/com/csci448/kduong/finalproject/MainActivity.java
@@ -1,16 +1,32 @@
 package com.csci448.kduong.finalproject;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mHomeButton;
+    private Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_container);
+
+        // This is to test the buttons created in main to see if the buttons in the fragment will show
+        /*mSearchButton = (Button) this.findViewById(R.id.search);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });*/
 
         if(savedInstanceState == null) {
             Fragment fragment = new HomeFragment();
