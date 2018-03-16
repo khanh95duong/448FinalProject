a23d58877bbcf3b214d1e5e63b7af7a501dfffc6
Khanh Duong <kduong@mymail.mines.edu>
2018-03-16 14:59:29 -0600

    Added FABs

--- app/src/main/java/com/csci448/kduong/finalproject/SettingFragment.java
+++ app/src/main/java/com/csci448/kduong/finalproject/SettingFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by darks on 3/14/2018.
@@ -12,10 +13,19 @@ import android.view.ViewGroup;
 
 public class SettingFragment extends Fragment {
 
+    private Button mLogout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_setting, container, false);
 
+        mLogout = (Button) v.findViewById(R.id.logout_button);
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
         return v;
     }
 }
