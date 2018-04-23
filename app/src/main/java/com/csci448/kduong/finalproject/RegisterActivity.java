dc268c696fdeabdd63e2864f22f0b9c6b6e30e3c
The Ngo <thngo@mymail.mines.edu>
2018-04-22 20:42:31 -0600

    Added the registration page and the firebase(database) to store the user login information

--- app/src/main/java/com/csci448/kduong/finalproject/RegisterActivity.java
+++ app/src/main/java/com/csci448/kduong/finalproject/RegisterActivity.java
@@ -1,6 +1,5 @@
 package com.csci448.kduong.finalproject;
 
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,8 +10,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,63 +22,63 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
-import java.net.MalformedURLException;
-
 import io.fabric.sdk.android.Fabric;
 
 /**
- * Created by The Ngo on 3/12/2018.
+ * Created by The Ngo on 4/22/2018.
  */
 
+public class RegisterActivity extends AppCompatActivity {
 
-public class LoginActivity extends AppCompatActivity {
-
-    private TextView mToRegister;
-    private Button mSubmitButton;
+    // data for registering user
+    private Button mRegister;
+    private TextView mToLogin;
     private EditText mUserName;
     private EditText mPassword;
 
+    // other variables
     private ProgressDialog mProgressDialog;
     private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.fragment_register);
         Fabric.with(this, new Crashlytics());
 
+        // authenticating the database
         mFirebaseAuth = FirebaseAuth.getInstance();
-        if(mFirebaseAuth.getCurrentUser() != null) {
-            finish();
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-        }
+
+        //progress dialog
         mProgressDialog = new ProgressDialog(this);
 
-        mUserName = (EditText) this.findViewById(R.id.user_name);
-        mPassword = (EditText) this.findViewById(R.id.password);
+        mUserName = (EditText) this.findViewById(R.id.register_user_name);
+        mPassword = (EditText) this.findViewById(R.id.register_password);
 
-        mSubmitButton = (Button) this.findViewById(R.id.login);
-        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+        mRegister = (Button) this.findViewById(R.id.register);
+        mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                userLogin();
+                registerUser();
             }
         });
 
-        mToRegister = (TextView) this.findViewById(R.id.to_register);
-        mToRegister.setOnClickListener(new View.OnClickListener() {
+        mToLogin = (TextView) this.findViewById(R.id.to_login);
+        mToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish(); // close the activity
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+
     }
 
-    // Log in after checking if everything is correct
-    public void userLogin() {
+    //Register user after checking if everything is correct
+    public void registerUser() {
+
+        // get string from the edit texts, not sure why there is trim, it is from the guide
         String username = mUserName.getText().toString().trim();
         String password = mPassword.getText().toString().trim();
 
@@ -93,20 +94,24 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         // if everything is good, show a progress bar while the data is uploaded to database (fancy)
-        mProgressDialog.setMessage("Login In ...");
+        mProgressDialog.setMessage("Registering User ...");
         mProgressDialog.show();
 
-        // Sign in
-        mFirebaseAuth.signInWithEmailAndPassword(username, password)
+        // adding user to the database
+        mFirebaseAuth.createUserWithEmailAndPassword(username, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             finish();
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);                        }
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Could not register the user", Toast.LENGTH_SHORT).show();
+                        }
+                        mProgressDialog.dismiss();
                     }
                 });
-
     }
+
 }
--- app/src/main/java/com/csci448/kduong/finalproject/SettingFragment.java
+++ app/src/main/java/com/csci448/kduong/finalproject/SettingFragment.java
@@ -1,11 +1,14 @@
 package com.csci448.kduong.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by darks on 3/14/2018.
@@ -14,17 +17,28 @@ import android.widget.Button;
 public class SettingFragment extends Fragment {
 
     private Button mLogout;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_setting, container, false);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        if(mFirebaseAuth.getCurrentUser() == null) {
+            getActivity().finish();
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+        }
+
         mLogout = (Button) v.findViewById(R.id.logout_button);
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mFirebaseAuth.signOut();
                 getActivity().finish();
-            }
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);            }
         });
         return v;
     }
